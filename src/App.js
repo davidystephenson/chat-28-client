d0c03a1f75d665509d28eb8a898893cc6550ce52
David Y. Stephenson <david@davidystephenson.com>
2019-08-09 16:23:51 +0200

    Connect store to components

--- src/App.js
+++ src/App.js
@@ -1,64 +1,55 @@
 /* global EventSource */
 
 import React from 'react'
-import * as request from 'superagent'
+import { allMessages } from './actions'
+import { connect } from 'react-redux'
+import MessageForm from './components/MessageForm'
+import UserForm from './components/UserForm'
 
 class App extends React.Component {
-  state = {
-    message: '',
-    messages: []
-  }
+  state = { message: '' }
 
   source = new EventSource('http://localhost:5000/stream')
 
   componentDidMount () {
     this.source.onmessage = (event) => {
-      console.log('event.data', event.data)
+      console.log('event.data test:', event.data)
 
       const messages = JSON.parse(event.data)
 
-      this.setState({ messages })
+      this.props.allMessages(messages)
     }
-  }
-
-  onSubmit = async (event) => {
-    event.preventDefault()
-
-    await request
-      .post('http://localhost:5000/message')
-      .send({ message: this.state.message })
-
-    this.setState({ message: '' })
-  }
-
-  onChange = (event) => {
-    const { value } = event.target
-
-    this.setState({ message: value })
   }
 
   render () {
     const messages = this
-      .state
+      .props
       .messages
       .map((message, index) => <p key={index}>
-        {message.text}
+        {message.user}: {message.text}
       </p>)
 
-    const form = <form onSubmit={this.onSubmit}>
-      <input
-        type='text'
-        value={this.state.message}
-        onChange={this.onChange}
-      />
-      <button type='submit'>Send</button>
-    </form>
-
     return <main>
-      {form}
+      <MessageForm user={this.props.user} />
+
+      <UserForm user={this.props.user} />
+
       {messages}
     </main>
   }
 }
 
-export default App
+function mapStateToProps (state) {
+  return {
+    messages: state.messages,
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = {
+  allMessages
+}
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(App)

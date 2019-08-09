31d573871e864b7d5dfa3480e2f4845fcab78411
David Y. Stephenson <david@davidystephenson.com>
2019-08-09 14:32:12 +0200

    Destructure message objects

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global EventSource */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import * as request from 'superagent'
+
+class App extends React.Component {
+  state = {
+    message: '',
+    messages: []
+  }
+
+  source = new EventSource('http://localhost:5000/stream')
+
+  componentDidMount () {
+    this.source.onmessage = (event) => {
+      console.log('event.data', event.data)
+
+      const messages = JSON.parse(event.data)
+
+      this.setState({ messages })
+    }
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault()
+
+    await request
+      .post('http://localhost:5000/message')
+      .send({ message: this.state.message })
+
+    this.setState({ message: '' })
+  }
+
+  onChange = (event) => {
+    const { value } = event.target
+
+    this.setState({ message: value })
+  }
+
+  render () {
+    const messages = this
+      .state
+      .messages
+      .map((message, index) => <p key={index}>
+        {message.text}
+      </p>)
+
+    const form = <form onSubmit={this.onSubmit}>
+      <input
+        type='text'
+        value={this.state.message}
+        onChange={this.onChange}
+      />
+      <button type='submit'>Send</button>
+    </form>
+
+    return <main>
+      {form}
+      {messages}
+    </main>
+  }
 }
 
-export default App;
+export default App
